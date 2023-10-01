aa19598c3152849eacde3626f09da3cbd4877b50
Michael Myers <michael.myers@wustl.edu>
2023-09-30 21:58:20 -0500

    Some work on morphology.jl

--- src/morphology.jl
+++ src/morphology.jl
@@ -1,27 +1,40 @@
 
-function fill_in_gaps!(p::Parcel, neigh::Vector{Vector})
-	candidates = # restricted list of neigh
+function fill_in_gaps!(p::Parcel, neigh::Vector{Vector{Int}})
+	candidates = union([neigh[v] for v in vertices(p)]...)
 	while true
-		add_inds = filter(x -> x .<= 1, [sum(.!p[x]) for x in candidates])
-		length(add_inds) > 0 || break
-		p[add_inds] .= true
+		add_inds = filter(x -> sum(.!p[neigh[x]]) .<= 1, candidates)
+		p2 = Parcel(add_inds; n = length(p))
+		sum(setdiff(p2, p)) > 0 || break
+		intersect!(p, p2)
 	end
 end
 
-function dilate!(p::Parcel, A::SparseMatrixCSC; limit::Int = Inf)
-	temp = setdiff(p, baddata)
-	border_verts = setdiff(A[:, temp].rowval, temp)
-	border_verts = border_verts[1:min(limit, length(border_verts))]
-	union!(p, border_verts)
+function dilate!(p::Parcel, A::SparseMatrixCSC; limit::Union{Nothing, Int} = nothing)
+	border_verts = setdiff(
+		unique(A[:, setdiff(p, baddata)].rowval),
+		vertices(p)
+	)
+	length(border_verts) > 0 || return
+	if !isnothing(limit) && length(border_verts) > limit
+		border_verts = border_verts[1:limit]
+	end
+	border = Parcel(border_verts; n = length(p))
+	union!(p, border)
 	return length(border_verts)
 end
 
-function erode!(p::Parcel, A::SparseMatrixCSC; limit::Int = Inf)
-	temp =  setdiff(p, baddata)
-	temp2 = findall(temp)
-	border_verts = temp2[A[temp, .!temp].rowval]
-	border_verts = border_verts[1:min(limit, length(border_verts))]
-	setdiff!(p, border_verts)
+function erode!(
+		p::Parcel, neigh::Vector{Vector{Int}}; limit::Union{Nothing, Int} = nothing
+	)
+	good_verts = findall(setdiff(p, baddata))
+	border_verts = good_verts[
+		[any([!(x in good_verts) for x in neigh[x]]) for x in good_verts]
+	]
+	if !isnothing(limit) && length(border_verts) > limit
+		border_verts = border_verts[1:limit]
+	end
+	border = Parcel(border_verts; n = length(p))
+	setdiff!(p, border)
 	return length(border_verts)
 end
 
@@ -37,7 +50,7 @@ function resize!(
 			Δ += n_added
 			i += 1
 		else
-			n_removed = erode!(p, A; limit = Δ)
+			n_removed = erode!(p, neigh; limit = Δ)
 			Δ -= n_removed
 			i += 1
 		end
