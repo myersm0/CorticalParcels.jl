1fec344b1b29d363cacbf06054ff14722f4bbe12
Michael Myers <michael.myers@wustl.edu>
2023-10-01 11:03:15 -0500

    Some refactoring of morphology.jl, added docstrings

--- src/morphology.jl
+++ src/morphology.jl
@@ -1,5 +1,11 @@
 
-function fill_in_gaps!(p::Parcel, neigh::Vector{Vector{Int}})
+"""
+    close_gaps!(p, neighbors)
+
+Given a `Parcel` `p` and an adjacency list `neighbors`, fill in gaps, if any,
+by finding vertices in `p` where all of its neighbors but one belong to `p`
+"""
+function close_gaps!(p::Parcel, neighbors::Vector{Vector{Int}})
 	candidates = union([neigh[v] for v in vertices(p)]...)
 	while true
 		add_inds = filter(x -> sum(.!p[neigh[x]]) .<= 1, candidates)
@@ -9,13 +15,16 @@ function fill_in_gaps!(p::Parcel, neigh::Vector{Vector{Int}})
 	end
 end
 
+"""
+    dilate!(p, A; limit = nothing)
+
+Perform a single pass of dilation on `Parcel` `p`, guided by adjacency matrix `A`;
+optionally specify a `limit::Int` on the number of new vertices that can be added
+"""
 function dilate!(
 		p::Parcel, A::SparseMatrixCSC; limit::Union{Nothing, Int} = nothing
 	)
-	border_verts = setdiff(
-		unique(A[:, setdiff(p, baddata)].rowval),
-		vertices(p)
-	)
+	border_verts = setdiff(unique(A[:, p].rowval), vertices(p))
 	length(border_verts) > 0 || return
 	if !isnothing(limit) && length(border_verts) > limit
 		border_verts = border_verts[1:limit]
@@ -25,43 +34,50 @@ function dilate!(
 	return length(border_verts)
 end
 
+"""
+    erode!(p, neighbors; limit = nothing)
+
+Perform a single pass of erosion on `Parcel` `p`, guided by adjacency list `neighbors`;
+optionally specify a `limit::Int` on the number of vertices that you want to remove
+"""
 function erode!(
-		p::Parcel, neigh::Vector{Vector{Int}}; limit::Union{Nothing, Int} = nothing
+		p::Parcel, neighbors::Vector{Vector{Int}}; limit::Union{Nothing, Int} = nothing
 	)
-	good_verts = findall(setdiff(p, baddata))
-	border_verts = good_verts[
-		[any([!(x in good_verts) for x in neigh[x]]) for x in good_verts]
+	verts = vertices(p)
+	border_verts = verts[
+		[any([!(x in verts) for x in neigh[x]]) for x in verts]
 	]
 	if !isnothing(limit) && length(border_verts) > limit
 		border_verts = border_verts[1:limit]
 	end
-	border = Parcel(border_verts; n = length(p))
-	setdiff!(p, border)
+	setdiff!(p, border_verts)
 	return length(border_verts)
 end
 
-function resize!(
-		p::Parcel, desired_size::Int, A::AbstractMatrix; maxiter::Int = 20
+"""
+	 resize!(p, desired_size; A, neighbors)
+
+Resize a `Parcel` `p`, guided by an adjacency matrix and an adjacency list, 
+by repeated dilation or erosion until `p` reaches `desired_size`
+"""
+function Base.resize!(
+		p::Parcel, desired_size::Int; A::AbstractMatrix, neigh::Vector{Vector{Int}}
 	)
 	curr_size = size(p)
 	Δ = curr_size - desired_size
-	i = 1
-	while Δ != 0 && i < maxiter 
+	while Δ != 0
 		if Δ < 0
-			n_added = dilate!(p, A; limit = abs(Δ))
-			Δ += n_added
-			i += 1
+			nchanged = dilate!(p, A; limit = abs(Δ))
+			Δ += nchanged
 		else
-			n_removed = erode!(p, neigh; limit = Δ)
-			Δ -= n_removed
-			i += 1
+			nchanged = erode!(p, neigh; limit = Δ)
+			Δ -= nchanged
 		end
-		i == maxiter && println("Warning: maxiter condition reached")
+		if nchanged == 0
+			println("Warning: could not achieve size $desired_size (stopped at $(size(p)))")
+			break
+		end
 	end
-	temp = setdiff(rotated_parcel, baddata)
-	border_verts = setdiff(A[:, temp].rowval, temp)
-	if length(border_verts) / length(temp) >= 3
-		p .= false
-	end
+	return size(p)
 end
 
