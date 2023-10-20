df9564c6e65211b13beeaaa4e8dd8e17120c157f
Michael Myers <michael.myers@wustl.edu>
2023-10-20 11:00:26 -0500

    Backtracked on new set ops fns; not ready for release

--- src/parcels.jl
+++ src/parcels.jl
@@ -122,23 +122,6 @@ Compute the number of member vertices shared between two `Parcel`s `p1`, `p2`
 """
 overlap(p1::Parcel, p2::Parcel) = p1.membership' * p2.membership
 
-overlap(p::Parcel, x::Union{Vector{Bool}, BitVector}) = p.membership' * x
-
-overlap(p::Parcel, px::Parcellation) =
-	return sum([overlap(p, px[k]) for k in keys(px)])
-
-overlap(px::Parcellation, p::Parcel) =
-	return overlap(p, px)
-
-function overlap(px::Parcellation)
-	counts = zeros(Int, length(px))
-	for k in keys(px)
-		counts .+= px[k].membership
-	end
-	overlap_inds = findall(counts .> 1)
-	return sum(counts[overlap_inds] .- 1)
-end
-
 """
     complement(p1::Parcel, p2::Parcel)
 
@@ -146,7 +129,6 @@ Compute the number of member vertices in `Parcel` `p1` not shared by those of `p
 """
 complement(p1::Parcel, p2::Parcel) = p1.membership' * .!p2.membership
 
-complement(p::Parcel, x::Union{Vector{Bool}, BitVector}) = p.membership' * .!x
 
 
 
